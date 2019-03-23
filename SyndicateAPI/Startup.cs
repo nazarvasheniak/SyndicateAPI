dbfd0e6ed408d4a59618307b678f2571ba99d3e8
nazarvasheniak <nazar.vasheniak@gmail.com>
2019-03-23 11:43:04 +0300

    add swagger

--- SyndicateAPI/Startup.cs
+++ SyndicateAPI/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
+using Swashbuckle.AspNetCore.Swagger;
 using SyndicateAPI.BusinessLogic;
 using SyndicateAPI.Storage;
 
@@ -27,7 +28,7 @@ namespace SyndicateAPI
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
             services.AddBuisnessServices();
-            services.AddNHibernate("Server=localhost;Port=3306;Uid=root;Pwd=admin;Database=goldio_exchange;SslMode=none;");
+            services.AddNHibernate("Server=localhost;Port=3306;Uid=root;Pwd=admin;Database=syndicate_db;SslMode=none;");
             services.AddCors();
 
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
@@ -46,6 +47,12 @@ namespace SyndicateAPI
                         ValidateIssuerSigningKey = true
                     };
                 });
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "Syndicate API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -65,6 +72,16 @@ namespace SyndicateAPI
                 builder.WithOrigins("*").AllowAnyHeader().AllowAnyMethod());
 
             app.UseAuthentication();
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Syndicate API V1");
+            });
 
             //app.UseHttpsRedirection();
             app.UseMvc();
