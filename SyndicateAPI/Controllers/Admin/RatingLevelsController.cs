816a19394520675d8ec6934821134411d3a5afbc
nazarvasheniak <nazar.vasheniak@gmail.com>
2019-03-25 15:47:02 +0300

    add rating levels controller

--- SyndicateAPI/Controllers/Admin/RatingLevelsController.cs
+++ SyndicateAPI/Controllers/Admin/RatingLevelsController.cs
@@ -6,10 +6,14 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using SyndicateAPI.BusinessLogic.Interfaces;
+using SyndicateAPI.Domain.Models;
+using SyndicateAPI.Models;
+using SyndicateAPI.Models.Request;
+using SyndicateAPI.Models.Response;
 
 namespace SyndicateAPI.Controllers.Admin
 {
-    [Route("api/[controller]")]
+    [Route("api/admin/[controller]")]
     [ApiController]
     [Authorize]
     public class RatingLevelsController : Controller
@@ -23,6 +27,130 @@ namespace SyndicateAPI.Controllers.Admin
         {
             AdminUserService = adminUserService;
             RatingLevelService = ratingLevelService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var user = AdminUserService.GetAll()
+                .FirstOrDefault(x => x.Login == User.Identity.Name);
+
+            if (user == null)
+                return Unauthorized(new ResponseModel
+                {
+                    Success = false,
+                    Message = "Unauthorized"
+                });
+
+            var ratingLevels = RatingLevelService.GetAll()
+                .Select(x => new RatingLevelViewModel(x))
+                .ToList();
+
+            return Ok(new DataResponse<List<RatingLevelViewModel>>
+            {
+                Data = ratingLevels
+
+            });
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetRatingLevel(long id)
+        {
+            var user = AdminUserService.GetAll()
+                .FirstOrDefault(x => x.Login == User.Identity.Name);
+
+            if (user == null)
+                return Unauthorized(new ResponseModel
+                {
+                    Success = false,
+                    Message = "Unauthorized"
+                });
+
+            var ratingLevel = RatingLevelService.Get(id);
+            if (ratingLevel == null)
+                return BadRequest(new ResponseModel
+                {
+                    Success = false,
+                    Message = "Rating level not found"
+                });
+
+            return Ok(new DataResponse<RatingLevelViewModel>
+            {
+                Data = new RatingLevelViewModel(ratingLevel)
+            });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateRatingLevel([FromBody] CreateRatingLevelRequest request)
+        {
+            var user = AdminUserService.GetAll()
+                .FirstOrDefault(x => x.Login == User.Identity.Name);
+
+            if (user == null)
+                return Unauthorized(new ResponseModel
+                {
+                    Success = false,
+                    Message = "Unauthorized"
+                });
+
+            var ratingLevel = RatingLevelService.GetAll()
+                .FirstOrDefault(x => x.Title.ToLower() == request.Title.ToLower());
+
+            if (ratingLevel != null)
+                return BadRequest(new ResponseModel
+                {
+                    Success = false,
+                    Message = $"Rating level with title '{request.Title}' already exist"
+                });
+
+            ratingLevel = RatingLevelService.GetAll()
+                .FirstOrDefault(x => x.PointsCount == request.PointsCount);
+
+            if (ratingLevel != null)
+                return BadRequest(new ResponseModel
+                {
+                    Success = false,
+                    Message = $"Rating level with points count '{request.PointsCount}' already exist"
+                });
+
+            ratingLevel = new RatingLevel
+            {
+                Title = request.Title,
+                PointsCount = request.PointsCount
+            };
+
+            RatingLevelService.Create(ratingLevel);
+
+            return Ok(new DataResponse<RatingLevelViewModel>
+            {
+                Data = new RatingLevelViewModel(ratingLevel)
+            });
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteRatingLevel(long id)
+        {
+            var user = AdminUserService.GetAll()
+                .FirstOrDefault(x => x.Login == User.Identity.Name);
+
+            if (user == null)
+                return Unauthorized(new ResponseModel
+                {
+                    Success = false,
+                    Message = "Unauthorized"
+                });
+
+            var ratingLevel = RatingLevelService.Get(id);
+            if (ratingLevel == null)
+                return BadRequest(new ResponseModel
+                {
+                    Success = false,
+                    Message = "Rating level not found"
+                });
+
+            RatingLevelService.Delete(ratingLevel.ID);
+
+            return Ok();
         }
     }
 }
---
+++ SyndicateAPI/Models/Request/CreateRatingLevelRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SyndicateAPI.Models.Request
+{
+    public class CreateRatingLevelRequest
+    {
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public int PointsCount { get; set; }
+    }
+}
