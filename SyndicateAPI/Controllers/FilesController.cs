72e5cb1809ec8008dd97408561031165ded57101
nazarvasheniak <nazar.vasheniak@gmail.com>
2019-03-25 12:38:16 +0300

    add files controller

---
+++ SyndicateAPI/Controllers/FilesController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SyndicateAPI.BusinessLogic.Interfaces;
+using SyndicateAPI.Models;
+using SyndicateAPI.Models.Request;
+using SyndicateAPI.Models.Response;
+
+namespace SyndicateAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class FilesController : Controller
+    {
+        private IFileService FileService { get; set; }
+
+        public FilesController([FromServices] IFileService fileService)
+        {
+            FileService = fileService;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetFile(long id)
+        {
+            var file = FileService.Get(id);
+            if (file == null)
+                return NotFound(new ResponseModel
+                {
+                    Success = false,
+                    Message = "File not found"
+                });
+
+            return Ok(new DataResponse<FileViewModel>
+            {
+                Data = new FileViewModel(file)
+            });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UploadFile([FromBody] UploadFileRequest request)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ SyndicateAPI/Models/Request/UploadFileRequest.cs
@@ -0,0 +1,18 @@
+﻿using SyndicateAPI.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SyndicateAPI.Models.Request
+{
+    public class UploadFileRequest
+    {
+        [Required]
+        public byte[] Base64 { get; set; }
+
+        [Required]
+        public FileType Type { get; set; }
+    }
+}
