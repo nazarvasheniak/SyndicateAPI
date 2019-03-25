969e808ae00a441c7a169297c78b84c39610ccac
nazarvasheniak <nazar.vasheniak@gmail.com>
2019-03-25 15:24:33 +0300

    Create RatingLevelsController.cs

---
+++ SyndicateAPI/Controllers/Admin/RatingLevelsController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SyndicateAPI.BusinessLogic.Interfaces;
+
+namespace SyndicateAPI.Controllers.Admin
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class RatingLevelsController : Controller
+    {
+        private IAdminUserService AdminUserService { get; set; }
+        private IRatingLevelService RatingLevelService { get; set; }
+
+        public RatingLevelsController([FromServices]
+            IAdminUserService adminUserService,
+            IRatingLevelService ratingLevelService)
+        {
+            AdminUserService = adminUserService;
+            RatingLevelService = ratingLevelService;
+        }
+    }
+}
