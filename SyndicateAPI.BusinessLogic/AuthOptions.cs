13d6be8da5c9b223c89182d8ecdd70a302e126a5
nazarvasheniak <nazar.vasheniak@gmail.com>
2019-04-01 11:19:10 +0300

    Update AuthOptions.cs

--- SyndicateAPI.BusinessLogic/AuthOptions.cs
+++ SyndicateAPI.BusinessLogic/AuthOptions.cs
@@ -8,8 +8,7 @@ namespace SyndicateAPI.BusinessLogic
         public const string ISSUER = "GOLD.IO_RPCAuthServer"; // издатель токена
         public const string AUDIENCE = "http://localhost:14534/"; // потребитель токена
         const string KEY = "jd645JHkdH348thdsf3ujd4wk";   // ключ для шифрации
-        //public const int LIFETIME = 10; // DEV - 10 минут
-        public const int LIFETIME = 43200; // время жизни токена - 120 минут
+        public const int LIFETIME = 260000; // время жизни в минутах
         public static SymmetricSecurityKey GetSymmetricSecurityKey()
         {
             return new SymmetricSecurityKey(Encoding.ASCII.GetBytes(KEY));
